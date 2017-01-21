d6aa7eb46c3d532f5d1db9a70a79a8eb31a35ca6
cjwoodru2 <cjwoodru2@gmail.com>
2017-01-21 10:16:46 -0500

    add rendering to the clock componenet. also, add styles to the clock component while beginnign the Countdown React component build

--- app/components/Clock.jsx
+++ app/components/Clock.jsx
@@ -1,9 +1,15 @@
 var React = require('react');
 
 var Clock = React.createClass({
+  getDefaultProps: function () {
+    totalSeconds: 0
+  },
+  propTypes: {
+    totalSeconds: React.PropTypes.number
+  },
   formatSeconds: function(totalSeconds) {
-    var minutes = Math.floor(totalSeconds / 60) ;
-    var seconds = totalSeconds % 60;
+    var minutes = Math.floor(totalSeconds / 60);
+    var seconds = totalSeconds % 60
 
     if(seconds < 10) {
       seconds = '0' + seconds;
@@ -11,13 +17,16 @@ var Clock = React.createClass({
     if(minutes < 10) {
       minutes = '0' + minutes;
     }
-      return minutes + ":" + seconds;
+      return minutes + ":" + seconds
 
   },
   render: function() {
+    var {totalSeconds} = this.props;
     return (
-      <div>
-
+      <div className="clock">
+        <span className="clock-text">
+          {this.formatSeconds(totalSeconds)}
+        </span>
       </div>
     );
   }
--- app/components/Countdown.jsx
+++ app/components/Countdown.jsx
@@ -1,12 +1,14 @@
 var React = require('react'),
-    Link  = require('react-router');
+    Link  = require('react-router'),
+    Clock = require('Clock');
 
 var Countdown = React.createClass({
   render: function() {
     return (
 
-      <div>
+      <div calassName="text-center">
         <h1 className="text-center page-title">Countdown Page!</h1>
+        <Clock totalSeconds={129}/>
       </div>
     );
   }
