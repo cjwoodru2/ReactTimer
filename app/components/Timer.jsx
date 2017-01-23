566c8b61b0941630f59929132dde659fd508b5f6
cjwoodru2 <cjwoodru2@gmail.com>
2017-01-22 19:35:36 -0500

    add function timer component

--- app/components/Timer.jsx
+++ app/components/Timer.jsx
@@ -1,12 +1,61 @@
-var React = require('react'),
-    Link  = require('react-router'),
-    Clock = require('Clock');
+var React         = require('react');
+var Clock         = require('Clock');
+var Controls      = require('Controls');
 
 var Timer = React.createClass({
-  render: function() {
+  getInitialState: function () {
+    return {
+      count: 0,
+      countdownStatus: 'paused'
+    };
+  },
+  componentDidUpdate: function (prevProps, prevState) {
+    if (this.state.countdownStatus !== prevState.countdownStatus) {
+      switch (this.state.countdownStatus) {
+        case 'started':
+          this.startTimer();
+          break;
+        case 'stopped':
+          this.setState({count: 0});
+          this.setState({
+            countdownStatus: 'paused'
+          });
+        case 'paused':
+          clearInterval(this.timer)
+          this.timer = undefined;
+          break;
+      }
+    }
+  },
+  componentWillUnmount: function() {
+    clearInterval(this.timer);
+    this.timer = undefined;
+  },
+  startTimer: function () {
+    this.timer = setInterval(() => {
+      var newCount = this.state.count + 1;
+      this.setState({
+        count: newCount
+      })
+    }, 1000);
+  },
+  handleSetCountdown: function (seconds) {
+    this.setState({
+      count: seconds,
+      countdownStatus: 'started'
+    });
+  },
+  handleStatusChange: function (newStatus) {
+    this.setState({countdownStatus: newStatus});
+  },
+  render: function () {
+    var {count, countdownStatus} = this.state;
+
     return (
       <div>
-        <h1 className="text-center page-title">Timer Page!</h1>
+        <h1 className="page-title">Timer App</h1>
+        <Clock totalSeconds={count}/>
+        <Controls onSet countdownStatus={countdownStatus} onStatusChange={this.handleStatusChange}/>
       </div>
     );
   }
